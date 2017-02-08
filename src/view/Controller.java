23ccf283170dc406bc95e31556f3a5a36445d7b6
Arturo Corro <arturo.f.corro@gmail.com>
2017-02-07 20:44:49 -0500

    Created first button action

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -1,7 +1,10 @@
 package view;
 
+import application.Library;
+import application.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
@@ -11,8 +14,14 @@ public class Controller {
 	private ObservableList<String> obsList;
 	
 	public void start() {
-	 obsList = FXCollections.observableArrayList ("Single", "Double", "Suite", "Family App");
-	 songList.setItems(obsList); 
+	 obsList = FXCollections.observableArrayList ("Single", "Double", "Suite", "Family App"); // replace with getList() later
+	 songList.setItems(obsList);
+	 songList.getSelectionModel().select(0);
 	}
+	
+    public void addSong(ActionEvent event) {
+       Song s = new Song("test");
+       
+    }
 
 }
